27aea212e3ffa35990f52719ea13199c937e47a7
Moisés Mariano <moises@softbuilder.com.br>
2021-04-02 21:03:46 -0300

    feat: create User & UserComic models

---
+++ Infra/Models/User.cs
@@ -0,0 +1,10 @@
+namespace ComicStoreApi.Infra.Models
+{
+    public class User : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Infra/Models/UserComic.cs
@@ -0,0 +1,9 @@
+namespace ComicStoreApi.Infra.Models
+{
+    public class UserComic
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ComicId { get; set; }
+    }
+}
