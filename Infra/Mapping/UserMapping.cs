af3b8ec30a65b2f060c6b799890ba13120aa00cb
Moisés Mariano <moises@softbuilder.com.br>
2021-04-02 22:58:27 -0300

    configure user mapping

---
+++ Infra/Mapping/UserMapping.cs
@@ -0,0 +1,22 @@
+using ComicStoreApi.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ComicStoreApi.Infra.Mapping
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(entity => entity.Id);
+            builder.Property(entity => entity.Nome);
+            builder.Property(entity => entity.Username);
+            builder.Property(entity => entity.Email);
+            builder.Property(entity => entity.Password);
+            builder.HasMany(user => user.UsersComics)
+                   .WithOne(comic => comic.User)
+                   .HasForeignKey(entity => entity.UserId);
+        }
+    }
+}
