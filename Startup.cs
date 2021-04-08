c00491a72b19f7ed8635ed42065c24ddec41a92b
Moisés Mariano <moises@softbuilder.com.br>
2021-04-08 19:19:12 -0300

    configura autenticacao na startup

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,14 @@
+using System.Text;
 using ComicStoreApi.Infra;
 using ComicStoreApi.Infra.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ComicStoreApi
 {
@@ -28,10 +31,29 @@ namespace ComicStoreApi
                     Configuration.GetSection("ConnectionStrings").GetSection("Default").Value
                 )
             );
-            
+
             services.AddCors();
 
             services.AddControllers().AddControllersAsServices();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             services.AddMvc();
         }
@@ -49,6 +71,8 @@ namespace ComicStoreApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
