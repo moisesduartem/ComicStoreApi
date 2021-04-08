bf858bcefeacc5023b399c3e0a8af5afa99a554c
Moisés Mariano <moises@softbuilder.com.br>
2021-04-08 18:36:45 -0300

    cria secret key

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace ComicStoreApi
+{
+    public static class Settings
+    {
+        public static string Secret = "652c7dc687d98c9889304ed2e408c74b611e86a40caa51c4b43f1dd5913c5cd0";
+    }
+}
