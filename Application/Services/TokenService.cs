e2daa6c15127695eb151a9bdb057275517ca807c
Moisés Mariano <moises@softbuilder.com.br>
2021-04-08 19:13:01 -0300

    cria metodo generate token no service

---
+++ Application/Services/TokenService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using ComicStoreApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace ComicStoreApi.Application.Services
+{
+    public class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials =
+                new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
